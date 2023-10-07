aba88590fbaae65d00b5be5ea8eeeb2ff07b5b6e
judejudo <86878327+judejudo@users.noreply.github.com>
2023-10-07 16:08:30 +0300

    Googlesheet API linked with nutrition API

--- day_38_googlesheets/main.py
+++ day_38_googlesheets/main.py
@@ -1,6 +1,7 @@
 from  dotenv import load_dotenv
 import os
 import requests
+from datetime import datetime
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -11,6 +12,8 @@ HEIGHT = os.getenv("HEIGHT")
 GENDER = os.getenv("GENDER")
 
 nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/755b3d11bce3f82747c13489820150d0/judeWorkouts/workouts"
+
 prompt = input("Please enter your days workout exercise ")
 headers = {
     "x-app-id": APP_KEY,
@@ -23,9 +26,27 @@ params = {
         "weight_kg":WEIGHT,
         "height_cm": HEIGHT
     }
+
+current_date = datetime.now()
+
 response = requests.post(nutrition_endpoint, headers=headers, json=params)
+json_response = response.json()
 
-# if response.status_code == 200:
-#     print(response.status_code)
 
-print(response.json())
+for exercise in json_response['exercises']:
+    sheety_data = {
+        "workout":{
+            "date": current_date.strftime("%d/%m/%Y"),
+            "time": current_date.strftime("%H:%M:%S"),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+
+sheety_response = requests.post(sheety_endpoint,json=sheety_data)
+print(sheety_response.json())
+
+if response.status_code == 200 and sheety_response.status_code ==200:
+    print("Successfully Created Row in you Googlesheety Data")
