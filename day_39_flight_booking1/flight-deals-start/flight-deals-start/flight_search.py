72d40e1dd8b70cebc4ed351fedea4ddc599e689a
judejudo <86878327+judejudo@users.noreply.github.com>
2023-10-11 01:20:42 +0300

    Get iata codes for each city via tequila api

--- day_39_flight_booking1/flight-deals-start/flight-deals-start/flight_search.py
+++ day_39_flight_booking1/flight-deals-start/flight-deals-start/flight_search.py
@@ -2,7 +2,14 @@ import requests
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-
-    def add_testing(self,cityname):
-        code = "TESTING"
-        return code
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+        
+        
+        
+    def add_iata_code(self,term,location_types,api_key):
+        response = requests.get(url = self.endpoint,params={
+                                                            "term": term,
+                                                            "location_types": location_types,
+                                                            },headers = {"apikey": api_key})
+        return response.json()
--- day_39_flight_booking1/flight-deals-start/flight-deals-start/main.py
+++ day_39_flight_booking1/flight-deals-start/flight-deals-start/main.py
@@ -2,30 +2,27 @@
 from data_manager import DataManager
 from pprint import pprint
 from flight_search import FlightSearch
+from  dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 SHEETY_ENDPOINT = "https://api.sheety.co/755b3d11bce3f82747c13489820150d0/copyOfFlightDeals/prices"
-FLIGHT_SEARCH_ENDPOINT = "https://tequila-api.kiwi.com" 
+FLIGHT_SEARCH_ENDPOINT = "https://api.tequila.kiwi.com/locations/query" 
 SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/755b3d11bce3f82747c13489820150d0/copyOfFlightDeals/prices"
-SHEETY_DELETE_ENDPOINT = "https://api.sheety.co/755b3d11bce3f82747c13489820150d0/copyOfFlightDeals/prices/"
-flight_api_key = "PCgbo1vt9rFZRgVSQE4dl1TUgPnSKGUz"
-
-
-parameters = {"flight_api_key":flight_api_key}
+API_KEY = os.environ.get("API_KEY")
 
 
 sheet_data = DataManager(SHEETY_ENDPOINT)
 sheet_data_update = DataManager(SHEETY_PRICES_ENDPOINT)
 list_of_data = sheet_data.get_data()
 sheet_city_names = DataManager(SHEETY_ENDPOINT)
-flight_search_data = FlightSearch()
 sheety_iatacode_update = DataManager(SHEETY_PRICES_ENDPOINT)
-sheet_data_delete = DataManager(SHEETY_DELETE_ENDPOINT)
+flight_search = FlightSearch(endpoint=FLIGHT_SEARCH_ENDPOINT)
 
 if list_of_data[0]["iataCode"] == "":
     for row in list_of_data:
-        row["iataCode"] = flight_search_data.add_testing(row["city"])
-    # print(f"sheet_data:\n {sheet_data}")
-        sheet_data_update.update_sheety(city=row["iataCode"], object_id=row["id"])
-        print(row["iataCode"])
+        response = flight_search.add_iata_code(term=row["city"],location_types="city",api_key=API_KEY)
+        iata_code = response["locations"][0]["code"]
+        sheet_data_update.update_sheety(city=iata_code, object_id=row["id"])
+
 
