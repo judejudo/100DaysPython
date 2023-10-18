6798d865162d4815eeb35601a1084802f991a995
judejudo <86878327+judejudo@users.noreply.github.com>
2023-10-18 15:33:37 +0300

    Web scrapping done!

--- day_45_webscrapping/main.py
+++ day_45_webscrapping/main.py
@@ -13,9 +13,25 @@ import requests
 # headings =soup.select(".heading")
 # print(headings)
 
-response = requests.get("https://news.ycombinator.com/news")
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 yc_web_page = response.text
 
+
 soup = BeautifulSoup(yc_web_page, "html.parser")
-name = soup.find(name="a", class_="storylink")
-print(name)
+movie_list = []
+sum = 100
+for item in soup.findAll(name="h3", class_="title"):
+    movie_name= item.get_text()
+    # print (movie_name)
+    if movie_name == "12: The Godfather Part II":
+        movie_list.append(f"{sum}: {movie_name.split(':', 1)[1].strip()}")
+    else:
+        movie_list.append(f"{sum}) {movie_name.split(')', 1)[1].strip()}")
+    sum = sum - 1
+
+# print(len(movie_list))
+reversed_movie_list = movie_list[::-1]
+
+with open("day_45_webscrapping/movies_file.txt", "w", encoding='utf-8') as file:
+    for movie in reversed_movie_list:
+        file.write(f"{movie}\n")
